cf55d1b1bded3e256a8d635262aac3eafa6d214b
mayur07-svg <mayurparmar1126@gmail.com>
2026-02-23 23:47:35 +0530

    swap two number

---
+++ swap.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+
+class swap{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number num :");
+        int num = sc.nextInt();
+
+        System.out.println("Enter the number num2 :");
+        int num2  = sc.nextInt();
+
+        System.out.println("Before value Num :"+num);
+        System.out.println("Before value Num2 :"+num2);
+        
+
+        int temp = num;
+        num = num2;
+        num2 = temp;
+        
+
+
+        System.out.println("After value Num :"+num);
+        System.out.println("After value Num2 :"+num2);
+
+        
+    }
+}
